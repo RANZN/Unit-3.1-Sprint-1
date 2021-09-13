1770bc7bbc7f59daec550759d5d68257c4184002
PRAKASH RANJAN <aryaprakashranjan@gmail.com>
2021-09-13 14:12:10 +0530

    Initital

---
+++ app/src/main/java/com/ranzan/unit31sprint_1/VideoPlayerActivity.java
@@ -0,0 +1,18 @@
+package com.ranzan.unit31sprint_1;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class VideoPlayerActivity extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_video_player_activity, container, false);
+    }
+}
